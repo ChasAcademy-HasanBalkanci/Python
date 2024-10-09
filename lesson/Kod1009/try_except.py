6433e2ea0c51c6d6231bd4d35a37ffb8c0df8414
ChasAcademy-HasanBalkanci <hasan.balkanci@chasacademy.se>
2024-10-09 13:15:26 +0200

    try_except was added.

---
+++ lesson/Kod1009/try_except.py
@@ -0,0 +1,13 @@
+def dela():
+    try:
+        tal1 = int(input("Ange ett tal: "))
+        tal2 = int(input("Ange ett annat tal: "))
+        resultat = tal1 / tal2
+        print(f"Resultatet av {tal1} / {tal2} är {resultat}")
+    except ZeroDivisionError:
+        print("Något gick fel, du kan inte dela med noll.")
+        dela()
+    except Exception as error:
+        print(f"Fel: {error}.")
+        dela()
+dela()
