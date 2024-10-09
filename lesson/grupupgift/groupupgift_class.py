c7cf279dcaa92ce3b4a099c35387bd75ec000bc0
ChasAcademy-HasanBalkanci <hasan.balkanci@chasacademy.se>
2024-10-09 10:52:56 +0200

    the work on class was added.

---
+++ lesson/grupupgift/groupupgift_class.py
@@ -0,0 +1,9 @@
+class Uppgift:
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+    # def __str__(self):
+    #     return f'Uppgift: X={self.X}, Y={self.Y}'
+    
+min_uppgift = Uppgift(10, 5)
+print(min_uppgift)
