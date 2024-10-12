ef546ebe16206ecf7077bf2fb6d363f6323752a1
ChasAcademy-HasanBalkanci <hasan.balkanci@chasacademy.se>
2024-10-13 00:34:41 +0200

    Co-authored-by: Daniel Holström <Messium@users.noreply.github.com>

---
+++ BTK_Akademi/list_iterable.py
@@ -0,0 +1,10 @@
+list_1 = [item for item in range(5)]
+print(list_1)
+
+list_2 = [item for item in range(1, 10) if item % 2 == 0 and item % 3 == 0]
+
+print(list_2)
+
+corperation = [item.upper() for item in "ChasAcademy"] 
+
+print(corperation)
