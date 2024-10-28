a8f4e0a4158b64b59ed2e5c8c80e6546ef2fe7c0
ChasAcademy-HasanBalkanci <hasan.balkanci@chasacademy.se>
2024-10-28 09:59:02 +0100

    json was added

--- lesson/Exercises/_json.py
+++ lesson/Exercises/_json.py
@@ -1,12 +1,43 @@
 import json
+import os
 
 person_string = '{"name": "Ali", "languages":["Python", "C#"]}'
 
 #Json to dictionary
-result = json.loads(person_string)
-print(type(result))
-print(result)
+# result = json.loads(person_string)
+# print(type(result))
+# os.getcwd()
+# os.chdir("D:\\GitHubChassAcadmey\\Python\\lesson\\Exercises")
+#with open("person.json", "r") as file:
+#    person = json.load(file)
+#    print(person)
+
+# result = person["languages"][1]
+# print(result)
 
 # result = person["languages"][1]
 # print(result)
 # print(person.get("name"))
+
+person_dict = {
+    "name": "Ali",
+    "languages": ["Python", "C++"],
+    "city": "Istanbul"
+}
+
+# result = json.dumps(person_dict)
+# print(type(result))
+# print(result)
+
+os.chdir("D:\\GitHubChassAcadmey\\Python\\lesson\\Exercises")
+# with open("person.json", "w", encoding="utf-8") as file:
+#     json.dump(person_dict, file, indent=4)
+#     print(f"Saved!")
+
+# with open("person.json", "r", encoding="utf-8") as file:
+#     person = json.load(file)
+#     print(person)
+
+person_dict_1 = json.loads(person_string)
+result = json.dumps(person_dict_1, indent=4, sort_keys=True)
+print(person_dict_1, "\n", result)
